316ace0942b4112d3558b5abff26981991fcfe4c
dragfire <asem.devajit@gmail.com>
2016-06-06 10:37:40 +0530

    Add Dashboard Admin

--- controllers/socketio.js
+++ controllers/socketio.js
@@ -2,11 +2,19 @@ var debug = require('debug')('Nashty: Socket IO');
 
 var sio = function (io) {
     io.on('connection', function (socket) {
-        debug(socket.id, socket.headers);
+        debug(socket.id);
+        //debug(socket.adapter.rooms);
+
+        // Role: Client
         socket.on('new user', function (data) {
-            debug(data);
-            socket.join('test');
-            socket.broadcast.to('test').emit('user joined', data);
+            debug(JSON.stringify(data, null,2));
+            socket.join(data.site);
+            io.in(data.site).emit('user joined', data);
+        });
+
+        socket.on('join admin', function (data) {
+            socket.join(data.site);
+            io.in(data.site).emit('admin joined', data);
         });
     });
 };
