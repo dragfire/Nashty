8d976cfc7ab16b9d52ec734474a96a7b85a5416d
dragfire <asem.devajit@gmail.com>
2016-06-08 22:07:14 +0530

    Remove bundle map # Enable Client Chat App

--- public/js/nashty.client.socketio.js
+++ public/js/nashty.client.socketio.js
@@ -1,6 +1,7 @@
 var socket = io('http://localhost:3000');
 var $clientMsg = $('.msg.sent');
-var $adminMsg =$('.msg.received');
+var $nashContent = $('.nash-content');
+var $adminMsg = $('.msg.received');
 var $textMsg = $('#nash-message');
 var $sendBtn = $('#nash-sendbtn');
 
@@ -8,4 +9,15 @@ socket.emit('new client', {role: 'client', site: $('#company').val()});
 
 socket.on('client joined', function (data) {
     console.log('Client joined', data);
+});
+
+$sendBtn.click(function () {
+    console.log('client:new message', {text: $textMsg.val()});
+    $nashContent.append('<div class="msg received">' + escape($textMsg.val()) + '</div><div class="clear"></div>');
+    socket.emit('client:new message', {text: $textMsg.val()});
+});
+
+socket.on('admin:message created', function (data) {
+    console.log('admin:message created', data);
+    $nashContent.append('<div class="msg sent">' + escape(data.text) + '</div><div class="clear"></div>');
 });
